3ca8b428ef0f91e031f25121be11d8510cfd7789
KaranChandekar <karanchandekar431@gmail.com>
2025-07-10 23:20:34 +0530

    add wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require("wezterm")
 -- This will hold the configuration
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font("JetBrainsMono Nerd Font Propo")
 config.font_size = 12.0
 
 config.enable_tab_bar = false
